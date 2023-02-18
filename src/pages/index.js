fffa2aa3987b1b445e2762549440ba1ed0f17cb1
Matt <matt@spectrebot.net>
2023-02-18 09:01:41 +0000

    remove broken font definer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+        
+        </>
+    )
 }
