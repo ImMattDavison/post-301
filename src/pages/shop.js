83072474675ab9c17a3c973794a9e8b2a1c7093f
Matt <matt@spectrebot.net>
2023-02-18 21:23:34 +0000

    import necessary vars

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -4,8 +4,11 @@ import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { useState, useEffect } from "react";
 import styles from "@/styles/Shop.module.css";
+import products from "../../public/products.json";
 
 const Shop = ({ subStatus, customers }) => {
+
+    const { user, error, isLoading } = useUser();
 
     if(!customers[0]){
         return(
@@ -18,9 +21,115 @@ const Shop = ({ subStatus, customers }) => {
         )
     }
 
+    const [ordered, setOrdered] = useState([])
+
+    useEffect(() => {
+        setOrdered(customers[0].ordered ? customers[0].ordered : [])
+    }, [])
+
+    const placeOrder = (productId) => {
+
+        return async () => {
+
+            const newOrder = ( 
+                {
+                    id: productId.id,
+                    name: productId.name,
+                    price: productId.price,
+                    image: productId.image,
+                    description: productId.description,
+                    price: productId.price,
+                    status: 'pending',
+                    date: new Date().toDateString(),
+                    ordnum: Math.floor(Date.now() / 1000)
+                }
+            )
+
+            console.log(ordered)
+
+            ordered.push(newOrder)
+
+            console.log(ordered)
+
+            const supabase = getSupabase(user.accessToken);
+
+            if(subStatus.length == 0 || subStatus[0].is_subscribed == false){
+                subStatus[0] && subStatus[0].is_subscribed == false ? (
+                    await supabase.from('is_subscribed').update([
+                        { is_subscribed: true }
+                    ]).eq('user_id', user.sub)
+                ) : (
+                    await supabase.from('is_subscribed').insert([
+                        { 
+                            user_id: user.sub, 
+                            is_subscribed: true 
+                        }
+                ]))
+            }
+
+            const { data, error } = await supabase
+                .from('customers')
+                .update({
+                    ordered: ordered
+                }).eq('user_id', user.sub)
+
+            window.location.reload()
+        }
+    }
+
     return (
         <>
-        
+            <div className={`${styles.shop} container`}>
+                <h1>Shop</h1>
+                <div>
+                    <h2>Top Picks at IKEA</h2>
+                    <div className={styles.products}>
+                        {products.slice(0,3).map((product) => (
+                            <div className={styles.product}>
+                                <img src={product.image} alt={product.name} />
+                                <span className={styles.price}>
+                                    <h3>{product.name}</h3>
+                                    <p>£{product.price}</p>
+                                </span>
+                                <p>{product.description}</p>
+                                <button className={styles.orderButton} onClick={placeOrder(product)}>ORDER</button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div>
+                    <h2>Top Picks at Nike</h2>
+                    <div className={styles.products}>
+                        {products.slice(3,6).map((product) => (
+                            <div className={styles.product}>
+                                <img src={product.image} alt={product.name} />
+                                <span className={styles.price}>
+                                    <h3>{product.name}</h3>
+                                    <p>£{product.price}</p>
+                                </span>
+                                <p>{product.description}</p>
+                                <button className={styles.orderButton} onClick={placeOrder(product)}>ORDER</button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div>
+                    <h2>Top Picks at Tesco</h2>
+                    <div className={styles.products}>
+                        {products.slice(6,9).map((product) => (
+                            <div className={styles.product}>
+                                <img src={product.image} alt={product.name} />
+                                <span className={styles.price}>
+                                    <h3>{product.name}</h3>
+                                    <p>£{product.price}</p>
+                                </span>
+                                <p>{product.description}</p>
+                                <button className={styles.orderButton} onClick={placeOrder(product)}>ORDER</button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
