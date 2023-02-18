9ad766789ac8e4ab2e1f1cfb9916a158303e6423
Matt <matt@spectrebot.net>
2023-02-18 10:59:51 +0000

    add testing data to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,22 @@
 import styles from '../styles/Home.module.css'
-import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import { getSupabase } from '../utils/supabase'
 import Link from 'next/link'
+import { useUser } from '@auth0/nextjs-auth0/client';
 import { useState, useEffect } from 'react'
 
-const Index = ({ user, error, isLoading }) => {
+export default function Index() {
+    const { user, error, isLoading } = useUser();
+
     if (isLoading) return <div>Loading...</div>;
     if (error) return <div>{error.message}</div>;
 
-    return(
-        <main>
-            <h1>Home</h1>
-            {user ? console.log(user) : console.log('no user')}
-        </main>
-    )
+    if (user) {
+        return (
+        <div>
+            Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+        </div>
+        );
+    }
+
+    return <a href="/api/auth/login">Login</a>;
 }
-
-export const getServerSideProps = withPageAuthRequired()
-
-export default Index
